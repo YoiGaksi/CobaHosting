21e95e9c63d127905353c269c2ed7a27a70ea7b5
YoiGaksi <86912365+YoiGaksi@users.noreply.github.com>
2023-09-27 11:18:56 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+ const toIG = document.getElementById('toIG');
+// Menambahkan event listener untuk mengarahkan pengguna saat tombol diklik
+ toIG.addEventListener('click', function() {
+            // Ganti URL dengan URL halaman yang ingin Anda tuju
+            window.location.href = 'https://www.instagram.com/ocrystalineo/'; 
+        });
+const toTT = document.getElementById('toTT');
+
+        // Menambahkan event listener untuk mengarahkan pengguna saat tombol diklik
+        toTT.addEventListener('click', function() {
+            // Ganti URL dengan URL halaman yang ingin Anda tuju
+            window.location.href = 'https://www.tiktok.com/@icrystallinei'; 
+        });
